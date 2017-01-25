539dd300207b2c1843c2c9c086b383c963286fd3
Julie Anne Kohler <julieakohler@gmail.com>
2017-01-25 13:40:15 -0700

    app steps

---
+++ app10-directive-image-hover/controller.js
@@ -0,0 +1,7 @@
+angular.module("picture").controller("picCtrl", function($scope) {
+
+  $scope.test = "test";
+
+  $scope.name = 'joe blank'
+
+});
---
+++ app10-directive-image-hover/directive.js
@@ -0,0 +1,12 @@
+angular.module('picture').directive('pictureDir', function(){
+  return {
+      restrict: 'E',
+      templateUrl: './pictureDir.html',
+      scope: {
+        urlPic: '@str'
+      },
+      link: function(scope, elem, attrs) {
+        console.log(scope.str);
+      }
+  }
+})
